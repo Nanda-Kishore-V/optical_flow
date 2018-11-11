e0f34b14bd53f8d2a4373755da57f0a7bc74a42e
Nanda <blazingkishore@gmail.com>
2018-11-11 12:49:08 -0500

    FeatureTranslation

--- 5_Project3B/estimateFeatureTranslation.py
+++ 5_Project3B/estimateFeatureTranslation.py
@@ -1,6 +1,7 @@
 import cv2
 from scipy import signal
-
+from scipy.interpolate import RegularGridInterpolator
+import numpy as np
 def blur(img):
     return cv2.blur(img, (5,5))
 
@@ -13,11 +14,31 @@ def return_derivatives(img):
     Iy = signal.convolve2d(I_gray, Gy, mode = 'same')
     return Ix, Iy
 
-def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
+def estimateFeatureTranslation(startX, startY, img1, img2):
     img1_cpy = img1.copy()
     img1_blur = blur(img1_cpy)
     img2_cpy = img2.copy()
     img2__blur = blur(img2_cpy)
     Ix, Iy = return_derivatives(img1_blur)
     It = img2-img1
+    Ix_func = RegularGridInterpolator((range(Ix.shape[0]), range(Ix.shape[1])), Ix, bounds_error = False, fill_value = None)
+    Iy_func = RegularGridInterpolator((range(Iy.shape[0]), range(Iy.shape[1])), Iy, bounds_error = False, fill_value = None)
+    It_func = RegularGridInterpolator((range(It.shape[0]), range(It.shape[1])), It, bounds_error = False, fill_value = None)
+
     i,j = np.meshgrid(np.arange(startX-4.5,startX+4.6,1), np.arange(startY-4.5,startY+4.6,1),indexing = 'ij')
+    window_indices = np.dstack((i,j))
+    Ix_window = Ix_func(window_indices)
+    Iy_window = Iy_func(window_indices)
+    It_window = It_func(window_indices)
+    A = np.zeros((2,2))
+    A[0,0] = np.sum(Ix_window*Ix_window)
+    A[0,1] = np.sum(Ix_window*Iy_window)
+    A[1,0] = np.sum(Iy_window*Ix_window)
+    A[1,1] = np.sum(Iy_window*Iy_window)
+    b = np.zeros((2,1))
+    b[0,0] = -np.sum(Ix_window*It_window)
+    b[1,0] = -np.sum(Iy_window*it_window)
+    uv = np.linalg.solve(A,b)
+    newX = startX + uv[0,:]
+    newY = startY + uv[1,:]
+    return newX, newY
--- 5_Project3B/object_tracking.py
+++ 5_Project3B/object_tracking.py
@@ -8,9 +8,15 @@ from draw_bounding_box import draw_bounding_box
 from get_features import get_features
 def objectTracking(filename):
     cap = cv2.VideoCapture(filename)
-
+    img1 = None
+    img2 = None
     while(cap.isOpened()):
         ret, frame = cap.read()
+        if img1 is None and img2 is None:
+            img1 = frame
+            continue
+        img2 = img1
+        img1 = frame
         if ret == False:
             break
         #scipy.misc.imsave('outfile.jpg', frame) for saving individual frames
@@ -29,6 +35,11 @@ def objectTracking(filename):
             #bb_img[x,y,1] = 0
             #bb_img[x,y,2] =  255
         cv2.imshow('frame', bb_img)
+        newX, newY = estimateFeatureTranslation(j_fps[11], i_fps[11],img1, img2)
+        bb_img[newY, newX,0] = 0
+        bb_img[newY, newX,1] = 0
+        bb_img[newY, newX,2] = 255
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
