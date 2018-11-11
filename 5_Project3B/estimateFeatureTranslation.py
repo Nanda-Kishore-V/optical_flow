c47f02942983f519e2d7ac3c72cd18f7b628cb0d
Nanda <blazingkishore@gmail.com>
2018-11-11 12:49:08 -0500

    Add interpolation.

--- 5_Project3B/estimateFeatureTranslation.py
+++ 5_Project3B/estimateFeatureTranslation.py
@@ -1,6 +1,8 @@
 import cv2
 from scipy import signal
 
+from scipy.interpolate import RegularGridInterpolator
+
 def blur(img):
     return cv2.blur(img, (5,5))
 
@@ -18,6 +20,11 @@ def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
     img1_blur = blur(img1_cpy)
     img2_cpy = img2.copy()
     img2__blur = blur(img2_cpy)
+
     Ix, Iy = return_derivatives(img1_blur)
+
+    Ix_func = RegularGridInterpolator((range(Ix.shape[0]),range(Ix.shape[1])), Ix, bounds_error=False,fill_value=None)
+    Iy_func = RegularGridInterpolator((range(Iy.shape[0]),range(Iy.shape[1])), Iy, bounds_error=False,fill_value=None)
     It = img2-img1
+    
     i,j = np.meshgrid(np.arange(startX-4.5,startX+4.6,1), np.arange(startY-4.5,startY+4.6,1),indexing = 'ij')
