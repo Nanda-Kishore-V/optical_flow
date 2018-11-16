0770b1a4502388a622fbd47b0437eb61688c4b5c
Nanda <blazingkishore@gmail.com>
2018-11-16 18:23:25 -0500

    Add iterations.

--- 5_Project3B/estimateFeatureTranslation.py
+++ 5_Project3B/estimateFeatureTranslation.py
@@ -11,32 +11,43 @@ def return_derivatives(img):
 def estimateFeatureTranslation(startX, startY, img1, img2):
     img1_cpy = img1.copy()
     img2_cpy = img2.copy()
+    img1_func = RegularGridInterpolator((range(img1_cpy.shape[0]), range(img1_cpy.shape[1])), img1_cpy, bounds_error = False, fill_value = None)
+    img2_func = RegularGridInterpolator((range(img2_cpy.shape[0]), range(img2_cpy.shape[1])), img2_cpy, bounds_error = False, fill_value = None)
 
     Ix, Iy = return_derivatives(img1_cpy)
-    It = img2-img1
     Ix_func = RegularGridInterpolator((range(Ix.shape[0]), range(Ix.shape[1])), Ix, bounds_error = False, fill_value = None)
     Iy_func = RegularGridInterpolator((range(Iy.shape[0]), range(Iy.shape[1])), Iy, bounds_error = False, fill_value = None)
-    It_func = RegularGridInterpolator((range(It.shape[0]), range(It.shape[1])), It, bounds_error = False, fill_value = None)
 
-    i,j = np.meshgrid(np.arange(startY-4.5,startY+4.6,1), np.arange(startX-4.5,startX+4.6,1),indexing = 'ij')
-    window_indices = np.dstack((i,j))
-    Ix_window = Ix_func(window_indices)
-    Iy_window = Iy_func(window_indices)
-    It_window = It_func(window_indices)
+    uv = np.array([[0],[0]])
+    iterations = 0
+    while True:
+        i,j = np.meshgrid(np.arange(startY-4.5,startY+4.6,1), np.arange(startX-4.5,startX+4.6,1),indexing = 'ij')
+        window_indices = np.dstack((i,j))
+        Ix_window = Ix_func(window_indices)
+        Iy_window = Iy_func(window_indices)
 
-    zero_window = np.zeros((10,10))
-    if(np.array_equal(zero_window, Ix_window) and np.array_equal(zero_window, Iy_window)):
-        print("bad case")
-        return startX, startY
-    A = np.zeros((2,2))
-    A[0,0] = np.sum(Ix_window*Ix_window)
-    A[0,1] = np.sum(Ix_window*Iy_window)
-    A[1,0] = np.sum(Iy_window*Ix_window)
-    A[1,1] = np.sum(Iy_window*Iy_window)
-    b = np.zeros((2,1))
-    b[0,0] = -np.sum(Ix_window*It_window)
-    b[1,0] = -np.sum(Iy_window*It_window)
-    uv = np.linalg.solve(A,b)
-    newX = startX + uv[0,:]
-    newY = startY + uv[1,:]
+        shifted_window_indices = np.dstack((i+uv[0,:], j+uv[1,:]))
+        It_window = img2_func(shifted_window_indices)-img1_func(window_indices)
+
+        zero_window = np.zeros((10,10))
+        if(np.array_equal(zero_window, Ix_window) and np.array_equal(zero_window, Iy_window)):
+            print("bad case")
+            return startX, startY
+        A = np.zeros((2,2))
+        A[0,0] = np.sum(Ix_window*Ix_window)
+        A[0,1] = np.sum(Ix_window*Iy_window)
+        A[1,0] = np.sum(Iy_window*Ix_window)
+        A[1,1] = np.sum(Iy_window*Iy_window)
+        b = np.zeros((2,1))
+        b[0,0] = -np.sum(Ix_window*It_window)
+        b[1,0] = -np.sum(Iy_window*It_window)
+        uv = np.linalg.solve(A,b)
+        startX = startX + uv[0,:]
+        startY = startY + uv[1,:]
+        iterations += 1
+        print(iterations)
+        if np.allclose(uv, np.array([[0],[0]]), rtol=0.5, atol = 0.5):
+            break
+
+    newX, newY = startX, startY
     return newX, newY
